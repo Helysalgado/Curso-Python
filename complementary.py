b5f8b88778f6e03fcf1ff298f13d393b303ef2ca
Hely Salgado <heladia@ccg.unam.mx>
2020-02-19 13:28:13 -0600

    Revisión
    1.- Falta el reverso a tu programa.  Checa el ejemplo que se dió en la tarea. 
    2.- Si el archivo trae secuencias en minúsculas, tu programa no funcionaría

--- complementary.py
+++ complementary.py
@@ -2,12 +2,17 @@
 #Se obtiene la cadena complementaria a Seq.
 Seq = open ("sequence.txt")
 
+# Si la secuencia viene en minusculas tu programa no funciona
+# aqui te faltaria agregar una instrucción
+
 Seq = Seq.replace('A','t')
 Seq = Seq.replace('T','a')
 Seq = Seq.replace('G','c')
 Seq = Seq.replace('C','g')
 
 Seq = Seq.upper()
+
+## el programa pide el reverso también. No lo calculas
 
 print('La cadena complementaria es:')
 print(Seq)
