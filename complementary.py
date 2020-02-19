158ae6ab92857cabc523b9f92443f36f94fa0847
Melii99 <61039340+Melii99@users.noreply.github.com>
2020-02-19 16:51:46 -0600

    Update complementary.py

--- complementary.py
+++ complementary.py
@@ -1,5 +1,5 @@
 #Se crea un programa que lee el archivo sequence, lo guarda en la var. Seq.
-#Se obtiene la cadena complementaria a Seq.
+#Se obtiene la cadena complementaria a Seq. y se le aplica la reversa
 Seq = open ("sequence.txt")
 
 Seq = Seq.replace('A','t')
@@ -8,6 +8,7 @@ Seq = Seq.replace('G','c')
 Seq = Seq.replace('C','g')
 
 Seq = Seq.upper()
+Seq = Seq[::-1] 
 
-print('La cadena complementaria es:')
+print('La cadena reversa complementaria es:')
 print(Seq)
