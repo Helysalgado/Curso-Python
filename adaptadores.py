bd28ac4abf51995b5448791e53527643f6c077de
Melii99 <61039340+Melii99@users.noreply.github.com>
2020-02-26 18:33:55 -0600

    Update adaptadores.py

--- adaptadores.py
+++ adaptadores.py
@@ -5,7 +5,7 @@ seq_no_adapt= []
 # 3. se itera sobre el mismo para eliminar los saltos de linea
 for lines in file:
   lines=lines.strip()
-# 4 se guardan las secuencias sin adaptadores(*para que concordaran los outputs los adaptadores son de 10 bp)
+# 4 se guardan las secuencias sin adaptadores(*para que concordaran los outputs los adaptadores son de 10 bp, no 14)
   seq_no_adapt.append(lines[9:])
 # 5. se imprimen en pantalla la longitud de cada secuencia
   print(len(lines[9:]))
